948b648279d304959a3adf30f1fab60e7c7fbd2d
pbrve <pbrve@dtu.dk>
2023-07-12 16:56:31 +1000

    added testthat

---
+++ tests/testthat/test-first.R
@@ -0,0 +1,14 @@
+# 
+
+obj = ctsmrTMB$new()
+testthat::expect_s3_class(obj,class=c("ctsmrTMB","R6"))
+
+# system equations
+testthat::expect_error(obj$add_systems(x ~ y))
+testthat::expect_error(obj$add_systems(x123 ~ y))
+testthat::expect_error(obj$add_systems(dx ~ 1 * dt + dw))
+testthat::expect_error(obj$add_systems(dx ~ y123))
+
+
+testthat::expect_no_error(obj$add_systems(dx ~ dw))
+testthat::expect_no_error(check_system_eqs(dx ~ y*dt + dw))
